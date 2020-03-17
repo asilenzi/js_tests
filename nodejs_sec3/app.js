e2092a84c207d6576f2436c80398607dd13f7519
Alessandro Silenzi <alessandro.silenzi@gmail.com>
2020-03-17 19:58:44 +0100

    assignment 2 part3

--- nodejs_sec3/app.js
+++ nodejs_sec3/app.js
@@ -7,18 +7,12 @@ const pages = require('./pages')
 const app = express();
 
 
-app.use('/', (req, res, next) => {
-    console.log(req);
-    next();
-});
+/*app.use('/', (req, res, next) => {
+ *    console.log(req);
+ *    next();
+ *});*/
 app.use('/create_user', pages.create_user);
 app.use('/users', pages.users);
 app.use('/', pages.index);
-
-app.use((req, res, next) => {
-        routes.handler(req, res);
-        // next();
-    }
-);
 
 app.listen(3000);
